cf04f68a215b3f90829309d19ca7f36bc5591ae0
Nareilson <narotkd@gmail.com>
2019-10-04 08:37:00 -0300

    feat: troca de tela Navigation

--- app/src/main/java/teste/m/hackturunicatolica2019/views/fragment/FragmentLogin.kt
+++ app/src/main/java/teste/m/hackturunicatolica2019/views/fragment/FragmentLogin.kt
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_splash.view.*
 
 import teste.m.hackturunicatolica2019.R
 
@@ -16,8 +18,12 @@ class FragmentLogin : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        val view =   inflater.inflate(R.layout.fragment_splash, container, false)
+        view.tvNovaConta.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_fragmentLogin_to_fragmentCadastroUser)
+        }
+
+        return view
     }
 
 }
