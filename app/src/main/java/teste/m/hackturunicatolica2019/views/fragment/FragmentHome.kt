8ee2cf926a6dd3d721314ea2f6a40497468e6d53
Nareilson <narotkd@gmail.com>
2019-10-04 14:21:18 -0300

    feat: rescunho da tela home user

--- app/src/main/java/teste/m/hackturunicatolica2019/views/fragment/FragmentHome.kt
+++ app/src/main/java/teste/m/hackturunicatolica2019/views/fragment/FragmentHome.kt
@@ -1,25 +1,23 @@
 package teste.m.hackturunicatolica2019.views.fragment
 
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-import kotlinx.android.synthetic.main.fragment_splash.view.*
 
 import teste.m.hackturunicatolica2019.R
 
-class FragmentLogin : Fragment() {
+class FragmentHome : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view =   inflater.inflate(R.layout.fragment_splash, container, false)
-        view.tvNovaConta.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_fragmentLogin_to_fragmentCadastroUser)
-        }
+        val view = inflater.inflate(R.layout.fragment_fragment_home, container, false)
+
 
         return view
     }
