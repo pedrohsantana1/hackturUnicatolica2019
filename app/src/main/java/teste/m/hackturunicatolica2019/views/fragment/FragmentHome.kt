6ef4fa9ccad4cac19a760e9f68619f3e4f7b5b91
Daniel Arrais <danielarraiscarvalho@gmail.com>
2019-10-05 03:00:47 -0300

    Insercao imagens

--- app/src/main/java/teste/m/hackturunicatolica2019/views/fragment/FragmentHome.kt
+++ app/src/main/java/teste/m/hackturunicatolica2019/views/fragment/FragmentHome.kt
@@ -21,7 +21,14 @@ class FragmentHome : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_fragment_home, container, false)
-        val adapter = AdapterListaHome(arrayListOf(ModelListaHome(R.drawable.ic_facebook,"Nome","Descricao nome"),ModelListaHome(R.drawable.ic_facebook,"Nome","Descricao nome"),ModelListaHome(R.drawable.ic_facebook,"Nome","Descricao nome")))
+        val adapter = AdapterListaHome(arrayListOf(
+            ModelListaHome(R.drawable.icon_graciosa ,"Praia da Graciosa", "Melhor por do sol", "Praia"),
+            ModelListaHome(R.drawable.icon_arnos,"Praia das Arnos","Praia na região norte", "Praia"),
+            ModelListaHome(R.drawable.icon_geodesico,"Centro Geodésico","Centro do Brasil", "Praça"),
+            ModelListaHome(R.drawable.icon_palacio,"Palácio Araguaia","Maior praça da america latina", "Praça"),
+            ModelListaHome(R.drawable.icon_evilson,"Cachoeira do Evilson","Conheça Taquaruçu", "Cachoeira"),
+            ModelListaHome(R.drawable.icon_macaco,"Cachoeira escorrega macaco","Conheça Taquaruçu", "Cachoeira")))
+
         view.recycleViewHome.layoutManager = LinearLayoutManager(activity)
         view.recycleViewHome.adapter = adapter
         return view
