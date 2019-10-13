597981dc714f839cf02fee017184f76608d49d7c
Nareilson <narotkd@gmail.com>
2019-10-13 18:31:13 -0300

    feat: pequenos ajustes de layout

--- app/src/main/java/teste/m/hackturunicatolica2019/adapter/AdapterFotos.kt
+++ app/src/main/java/teste/m/hackturunicatolica2019/adapter/AdapterFotos.kt
@@ -4,13 +4,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.layout_item_fotos.view.*
+import kotlinx.android.synthetic.main.layout_item_maps.view.*
 import teste.m.hackturunicatolica2019.R
 import teste.m.hackturunicatolica2019.model.ModelListFotos
 
 class AdapterFotos(var list: ArrayList<ModelListFotos>):RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_item_fotos,parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_item_maps,parent,false)
         return  ViewModel(view)
     }
 
