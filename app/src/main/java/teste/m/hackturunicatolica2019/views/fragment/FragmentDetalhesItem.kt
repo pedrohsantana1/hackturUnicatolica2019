dfc8fa66d887663104c555f6f1100c4be58e680e
Nareilson <narotkd@gmail.com>
2019-10-13 22:56:20 -0300

    feat: maps inserido e funcional

--- app/src/main/java/teste/m/hackturunicatolica2019/views/fragment/FragmentDetalhesItem.kt
+++ app/src/main/java/teste/m/hackturunicatolica2019/views/fragment/FragmentDetalhesItem.kt
@@ -6,32 +6,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.fragment_fragment_detalhes_item.view.*
 import kotlinx.android.synthetic.main.layout_agencias.view.*
 import kotlinx.android.synthetic.main.layout_descricao.view.*
 import kotlinx.android.synthetic.main.layout_descricao.view.tvDescricao
-import kotlinx.android.synthetic.main.layout_fotos.view.*
-import kotlinx.android.synthetic.main.layout_item_lista_local.view.*
-
 import teste.m.hackturunicatolica2019.R
 import teste.m.hackturunicatolica2019.adapter.AdapterAgencias
-import teste.m.hackturunicatolica2019.adapter.AdapterFotos
 import teste.m.hackturunicatolica2019.model.ModelListAngencias
-import teste.m.hackturunicatolica2019.model.ModelListFotos
 
 class FragmentDetalhesItem : Fragment() {
-
+    private lateinit var mMap: GoogleMap
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         val view = inflater.inflate(R.layout.fragment_fragment_detalhes_item, container, false)
         val viewDescricao = inflater.inflate(R.layout.layout_descricao, container, false)
-        val viewMaps = inflater.inflate(R.layout.layout_fotos, container, false)
+        val viewMaps = inflater.inflate(R.layout.layout_map, container, false)
         val viewAgencias = inflater.inflate(R.layout.layout_agencias, container, false)
-
-
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync { googleMap ->
+            googleMap?.let {
+                mMap = it
+            }
+            val sydney = LatLng(-34.0, 151.0)
+            mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        }
         viewAgencias.recycleAgencias.layoutManager = LinearLayoutManager(activity)
         viewAgencias.recycleAgencias.adapter = AdapterAgencias(
             arrayListOf(
@@ -60,5 +67,6 @@ class FragmentDetalhesItem : Fragment() {
         }
         return view
     }
+
 
 }
